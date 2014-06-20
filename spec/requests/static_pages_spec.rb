56f8c22f0e327b4f3338b4ea65d86fcbed71d8d0
Paul Calnon <paul.calnon@gmail.com>
2014-06-20 17:08:03 -0500

    finished layout and routes.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,48 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Samich'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Samich')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Samich Application | Home")
-    end
-
+    it { should have_content('Samich App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Samich Application | Help")
-    end
-
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Samich Application | About")
-    end
-
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 end
-
-
