cc480d7c1ed4df573626796285751430f5e2682f
Paul Calnon <paul.calnon@gmail.com>
2014-06-24 10:05:01 -0500

    created basic user data model and added secure passwords.

---
+++ db/migrate/20140620230641_add_password_digest_and_username_to_users.rb
@@ -0,0 +1,6 @@
+class AddPasswordDigestAndUsernameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+    add_column :users, :username, :string
+  end
+end
